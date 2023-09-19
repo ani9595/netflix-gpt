4bfd4e87271f0a167d30b7050a021d705bd02310
Aniruddha Khadse <aniruddha.khadse007>
2023-09-19 17:50:53 +0530

    Cards and geners

---
+++ src/hooks/usePopularMovies.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addPopularMovies } from "../utils/movieSlice";
+import { API_OPTIONS } from "../utils/constant";
+
+const usePopularMovies = () => {
+  const dispatch = useDispatch();
+  const popularMovies = useSelector((store) => store.movie?.popularMovies);
+
+  useEffect(() => {
+    !popularMovies && getPopularMovies();
+  }, []);
+
+  const getPopularMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    dispatch(addPopularMovies(json.results));
+  };
+};
+
+export default usePopularMovies;
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -9,3 +9,5 @@ export const API_OPTIONS = {
 
 export const API_GET_MOVIES =
   "https://api.themoviedb.org/3/movie/now_playing?page=1";
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
